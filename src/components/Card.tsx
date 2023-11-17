bbaafcd4267bd7648f417c08ca5f5a8aa85369d7
Betsue Weldemariam <btsegay0@gmail.com>
2023-11-17 18:22:53 +0300

    Layered items applied

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,18 +5,25 @@ type CardProps = {
   short_desc: string
 }
 
+const opaque = {
+  background: {
+    backgroundColor: "white",
+  },
+}
+
 function Card(props: CardProps) {
   return (
     <div
       className="frame flex flex-col items-center w-[367px] min-h-[579px] space-y-[30px] px-[24px] 
-      py-[30px] border-solid border-2 rounded-[5px] border-border_color hover:cursor-pointer"
+      py-[30px] border-solid border-2 rounded-[5px] border-border_color hover:cursor-pointer text-justify"
+      style={opaque.background}
     >
       <div className="image w-[186px] h-[192px]">
         <img src={props.img_src} alt={props.img_alt} />
       </div>
-      <div className="title font-cursive text-[30px]">{props.title}</div>
+      <div className="title font-cursive text-[30px] ">{props.title}</div>
       <div className="separator w-[317px] border-separator_color border-[1px]"></div>
-      <div className="short-description font-sans text-[20px] text-justify">
+      <div className="short-description font-sans text-[20px]">
         {props.short_desc}
       </div>
     </div>
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,59 @@
 function Layout({ children }: { children: React.ReactNode }) {
+  const emojis = [
+    "😀",
+    "😁",
+    "😅",
+    "🤣",
+    "😂",
+    "🙃",
+    "😉",
+    "😊",
+    "😇",
+    "😍",
+    "😘",
+    "😋",
+    "😜",
+    "😝",
+    "🤭",
+    "😏",
+    "🤤",
+    "😴",
+    "🥳",
+    "🥸",
+    "😎",
+    "🤓",
+    "😮",
+    "😳",
+    "🥹",
+    "🥺",
+    "😱",
+    "😈",
+    "😖",
+    "😱",
+    "😭",
+  ]
   const backgroundImage = {
     addImage: {
-      backgroundRepeat: "no-repeat",
       backgroundColor: "rgba(255,255,255,0.85)",
     },
   }
+
+  emojis.map((emoji) => {
+    console.log(emoji)
+  })
+
   return (
     <>
-      <div className="w-full h-full bg-fixed" style={backgroundImage.addImage}>
-        <main className="my-[30px] mx-[250px]">{children}</main>
+      <div className="emoji-list fixed my-[30px] -z-10 w-screen h-screen flex text-[48px] gap-x-[60px] overflow-hidden">
+        {emojis.map((emoji) => {
+          return <div>{emoji}</div>
+        })}
       </div>
+      <div
+        className="w-screen h-screen my-[30px] fixed z-10"
+        style={backgroundImage.addImage}
+      ></div>
+      <main className="my-[30px] mx-[250px] absolute z-20">{children}</main>
     </>
   )
 }
