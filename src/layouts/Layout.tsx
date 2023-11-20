dacc4e442bf76e27a3b01d0ca2ccc6d0ed7f5450
Betsue Weldemariam <btsegay0@gmail.com>
2023-11-20 10:29:04 +0300

    Background emojis are generated randomly

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react"
+
 function Layout({ children }: { children: React.ReactNode }) {
+  const screenHeight = window.screen.height
+  const [iterator, setIterator] = useState([])
   const emojis = [
     "😀",
     "😁",
@@ -38,19 +42,38 @@ function Layout({ children }: { children: React.ReactNode }) {
     },
   }
 
-  emojis.map((emoji) => {
-    console.log(emoji)
-  })
+  function createArray() {
+    const iteration = screenHeight / 98
+    let temp = []
+    for (let i = 0; i < iteration; i++) {
+      temp.push(1)
+    }
+    setIterator(temp)
+  }
+
+  useEffect(() => {
+    createArray()
+  }, [])
+
+  // emojis.map((emoji) => {
+  //   console.log(emojis.length)
+  // })
 
   return (
     <>
-      <div className="emoji-list fixed my-[30px] -z-10 w-screen h-screen flex text-[48px] gap-x-[60px] overflow-hidden">
-        {emojis.map((emoji) => {
-          return <div>{emoji}</div>
+      <div className="emoji-list fixed mt-[-10px] my-[50px] -z-10 w-screen h-screen flex flex-col overflow-hidden">
+        {iterator.map((i) => {
+          return (
+            <div className="flex text-[48px] mt-[50px] gap-x-[50px]">
+              {emojis.map((emoji) => {
+                return <div>{emojis[Math.floor(Math.random() * 30)]}</div>
+              })}
+            </div>
+          )
         })}
       </div>
       <div
-        className="w-screen h-screen my-[30px] fixed z-10"
+        className="w-screen h-screen fixed z-10"
         style={backgroundImage.addImage}
       ></div>
       <main className="my-[30px] mx-[250px] absolute z-20">{children}</main>
