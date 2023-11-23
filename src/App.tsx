60509b52bf23fcd43a149c0d2fb79577fafb528b
Betsue Weldemariam <btsegay0@gmail.com>
2023-11-23 08:42:07 +0300

    Moved all routing to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,4 @@
-import Home from "./pages/Home"
-import Layout from "./layouts/Layout"
-import { Route, Routes, redirect } from "react-router-dom"
-import FortuneCookie from "./pages/FortuneCookie"
-import { useEffect } from "react"
-
 function App() {
-  useEffect(() => {
-    redirect("/")
-  }, [])
-
   return <></>
 }
 
