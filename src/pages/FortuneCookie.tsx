550306c2ea75becc05b083a3ff85bab783480e42
Betsue Weldemariam <btsegay0@gmail.com>
2024-07-09 15:48:50 +0300

    path change

--- src/pages/FortuneCookie.tsx
+++ src/pages/FortuneCookie.tsx
@@ -49,7 +49,7 @@ function FortuneCookie() {
         <div id="content-1" className="content-1">
           <AdviceContent
             heading="1 - Fortune Cookie Wonders"
-            img_src="/src/assets/cookie.svg"
+            img_src="../assets/cookie.svg"
             img_alt="cookie"
             deatil="Don't underestimate the power of dessert with a surprise 
           message inside. Remember, while dining at your favorite Chinese spot,
@@ -59,7 +59,7 @@ function FortuneCookie() {
         <div id="content-2" className="hidden content-2">
           <AdviceContent
             heading="2 - The Art of Interpretation"
-            img_src="/src/assets/the_art_of_interpetation.svg"
+            img_src="../assets/the_art_of_interpetation.svg"
             img_alt="A hand holding fortune cookie"
             deatil={`The authentic way to navigate through life with fortune cookies is by interpreting each message creatively. If it says, "You'll have a fantastic journey," it absolutely means, "book a one-way trip to Hawaii." And when it reads, "Tomorrow will bring a delightful surprise," definitely put in a leave application. You don't want to ruin the surprise by being stuck at work!`}
           />
@@ -67,7 +67,7 @@ function FortuneCookie() {
         <div id="content-3" className="hidden content-3">
           <AdviceContent
             heading="3 - Sharing is Caring... or a Trap?"
-            img_src="../src/assets/sharing_is_caring_or_a_trap.svg"
+            img_src="../assets/sharing_is_caring_or_a_trap.svg"
             img_alt="Kids sharing a fortune cookie"
             deatil="Some adhere to the belief of never sharing fortune cookies. But what if the 'Cookie of the Day' is the key to decode a prophecy? There's a simple solution to this — order extra cookies, but avoid squabbles over the ones foretelling the most cash. They are often misprints!"
           />
@@ -75,7 +75,7 @@ function FortuneCookie() {
         <div id="content-4" className="hidden content-4">
           <AdviceContent
             heading="4 - Cookie Fortune Luck vs Real Life"
-            img_src="../src/assets/luck_vs_real_life.svg"
+            img_src="../assets/luck_vs_real_life.svg"
             img_alt="Fortune cookie wishing a good luck"
             deatil="Lastly, keep a sense of humor. If your cookie has no fortune — it doesn't mean you're fortune-less in life! Similarly, if there's a quote about wisdom, it doesn't mean take up philosophy. Silly cookie, it's confusing wisdom with wisdom teeth!"
           />
@@ -83,7 +83,7 @@ function FortuneCookie() {
         <div id="content-5" className="hidden content-5">
           <AdviceContent
             heading="In conclusion"
-            img_src="../src/assets/fortune_cookie.svg"
+            img_src="../assets/fortune_cookie.svg"
             img_alt="Fortune cookie"
             deatil="Remember, life is all about the fun and laughter that comes along the way. And what's funnier than taking life advice from a cookie that can't even decide if it's a dessert or an appetizer!"
           />
